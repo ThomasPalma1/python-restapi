f1e57567895df73602b56b7f7e4abb8281acd29f
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-08-12 15:43:28 -0300

    changing the app
    - creating new functions;
    - adding new methods;
    - creating list.

--- app.py
+++ app.py
@@ -2,10 +2,45 @@ from flask import Flask
 
 app = Flask(__name__)
 
+store = [
+    {
+        'name': 'Zekrom Store',
+        'items': [
+            {
+                'name': 'Random Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
 
-@app.route('/')  # home page of the application
-def home():
-    return "Hello, world!"
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_stores(name):
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
 
 
 app.run(port=5000)
+
+# POST - used to receive data
+# GET - used to send data back only
