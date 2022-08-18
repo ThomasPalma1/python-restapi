62fdb394d51673aefcc435b7722782fb6abef513
Thomas Palma <thomaspalma777@gmail.com>
2022-08-18 16:19:39 -0300

    improving code and error control

--- code/app.py
+++ code/app.py
@@ -4,21 +4,22 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-items = []
+items = []  # in-memory database
 
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-            return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
-    def post(self, name):
-        data = request.get_json(force=True)
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
+
+def post(self, name):
+    if next(filter(lambda x: x['name'] == name, items), None) is not None:
+        return {'message': "An item with name '{}' already exists.".format(name)}, 400
+    data = request.get_json(force=True)
+    item = {'name': name, 'price': data['price']}
+    items.append(item)
+    return item, 201
 
 
 class ItemList(Resource):
