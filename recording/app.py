919070005ac9816236ebc872320795091ccb6f8f
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-10-29 19:15:02 -0300

    creating new methods
    - method to update item;
    - method to delete an existing store;

--- recording/app.py
+++ recording/app.py
@@ -47,7 +47,7 @@ def create_item():
             item_data["name"] == item["store_id"]
             and item_data["store_id"] == item["store_id"]
         ):
-            abort(404, message=f"Item already exists")
+            abort(404, message=f"Item already exists!")
     if item_data["store_id"] not in stores:
         abort(404, message="Store not found!")
 
@@ -78,6 +78,29 @@ def delete_item(item_id):
     try:
         del items[item_id]
         return {"message": "Item deleted!"}
+    except KeyError:
+        abort(404, message="Item not found!")
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted!"}
+    except KeyError:
+        abort(404, message="Store not found!")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price', and 'name' are included in the JSON payload!")
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
     except KeyError:
         abort(404, message="Item not found!")
 
